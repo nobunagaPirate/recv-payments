80c2bd0c5e5304d9a1f929ad98034ea42fd36429
LuKaz <andresfeliperu@gmail.com>
2021-07-04 07:45:24 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module receive-payables
+
+go 1.16
+
+require github.com/btcsuite/btcd v0.22.0-beta // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("asdf")
+}
+
+
+
+--start-mining  53w9jR --mining-threads  2 --bg-mining-enable
